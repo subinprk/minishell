b6fd2de7d1d9e5a26ad7386787468fba060f54e7
Subin Park <subpark@c4a9c10.42berlin.de>
2024-01-10 18:21:11 +0100

    removing some extra command

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 17:48:01 by subpark           #+#    #+#             */
-/*   Updated: 2024/01/09 17:52:23 by subpark          ###   ########.fr       */
+/*   Updated: 2024/01/10 18:21:01 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,40 +75,3 @@ int main(int argc, char **argv, char **envs)
 	free_2d(envp);
 	//exit_shell();
 }
-/*
-int main(int argc, char **argv, char **envs)
-{
-//	char	**line;
-	int		index, i;
-	char	*tmp;
-	char	**envp;
-	t_cmd	*tree;
-
-//	line = NULL;
-	if (argc && argv)
-
-//subin: should be initialised as a function for norminette later
-	i = 0;
-	index = 0;
-	g_envp = cpy_full_2d_array(envs);
-	envp = paths_array(envs);
-	set_signal();
-
-	while(argc && argv[0] != NULL i < 5)
-	{
-		//get_line(line);
-		//tree = extract_command(line[index]);
-		generate_prompt();
-		tmp = readline(" ");
-		tree = extract_command(tmp);
-		search_tree(tree, envp);
-		index ++;
-		free(tmp);
-		free_tree(tree);
-		i++;
-	}
-//also freeing shoud be in different function
-	free_2d(envp);
-	free_2d(g_envp);
-	exit(g_exit_status);
-}*/
