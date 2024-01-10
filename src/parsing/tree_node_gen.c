d8c88895823dd4795b2bcad4d4ecf502d0e15576
Subin Park <subpark@c4a9c10.42berlin.de>
2024-01-10 14:29:45 +0100

    fixed some redirection+pipe without command

--- src/parsing/tree_node_gen.c
+++ src/parsing/tree_node_gen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tree_node_gen.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: siun <siun@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 16:52:28 by siun              #+#    #+#             */
-/*   Updated: 2023/12/04 00:12:53 by siun             ###   ########.fr       */
+/*   Updated: 2024/01/10 14:27:10 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,9 @@ t_cmd	*generate_end_node(char **line, int node_type, int start, int end)
 	new_node->node_type = node_type;
 	new_node->left_child = NULL;
 	new_node->right_child = NULL;
+	if ((node_type == NODE_FILE_PATH || node_type == NODE_ARGV)
+		&& (line[start][0] == '<' || line[start][0] == '>'))
+		new_node->cmdstr = NULL;
 	new_node->cmdstr = text_array_part_cpy(line, start, end);
 	new_node->pipe_exist = -1;
 	return (new_node);
